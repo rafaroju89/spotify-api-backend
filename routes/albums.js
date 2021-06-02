a104397340f7fb1881341f1bbe4d98c37cb19dc4
rafaroju89 <rafaroju89@gmail.com>
2021-06-02 13:35:06 -0500

    Proyect Finished

--- routes/albums.js
+++ routes/albums.js
@@ -3,13 +3,13 @@ const {check} = require('express-validator');
 const {validateFields} = require('../middlewares');
 const {getAlbums, getAlbumDetails} = require('../controllers/albums');
 
-/*API method that handles the http://localhost:8081/v1/albums/${album} */
+/*GET API method that handles the http://localhost:8081/v1/albums/${album} */
 router.get('/:album',[
     check('album','The parameter "album" is required').not().isEmpty(),
     validateFields
 ], getAlbums);
 
-/*API method that handles the http://localhost:8081/v1/albums/details/${id}*/
+/*GET API method that handles the http://localhost:8081/v1/albums/details/${id}*/
 router.get('/details/:id',[
     check('id','The parameter "id" is required').not().isEmpty(),
     validateFields
